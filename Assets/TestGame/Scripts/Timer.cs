e494b8f7207c7a25746cf2c05d7ddaf4e1d07a5f
Bartosz Wężyk <bartoszwezykmail@gmail.com>
2023-05-17 12:09:03 +0200

    Progress fixed, timer and new game issues

--- Assets/TestGame/Scripts/Timer.cs
+++ Assets/TestGame/Scripts/Timer.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Timer : MonoBehaviour
 {
-    public float timeLeft = 60.0f; // The amount of time in seconds
+    public float timeLeft; // The amount of time in seconds
     Text timerText;             // The UI Text object to display the timer
     TestLevel testLevel;
-
+     
     private void Start()
     {
         timerText = GetComponent<Text>();
@@ -16,14 +18,24 @@ public class Timer : MonoBehaviour
     void Update()
     {
         timeLeft -= Time.deltaTime; // Subtract the elapsed time since last frame
-        timerText.text = "Time left: " + Mathf.Round(timeLeft).ToString(); // Update the UI text
+        GUI(timeLeft);
+        
+        
+        
 
         if (timeLeft < 0)
         {
             testLevel.GameLost();
+            
         }
     }
 
+    void GUI(float timeLeft)
+    {
+        float minutes = Mathf.Floor(timeLeft / 60);
+        float seconds = Mathf.RoundToInt(timeLeft % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
     public void SetTimer(float time)
     {
         timeLeft = time;
