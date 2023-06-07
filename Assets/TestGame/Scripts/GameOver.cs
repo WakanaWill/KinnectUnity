e61c49ea3d84273815e8bb1bc04db0be99b58774
Vladyslav Dovhalskyy <vlad32cj@gmail.com>
2023-06-07 18:31:49 +0200

    Set cursor visible on gameover pop-up

--- Assets/TestGame/Scripts/GameOver.cs
+++ Assets/TestGame/Scripts/GameOver.cs
@@ -5,13 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
     public void Return()
     {
+        Cursor.visible = true;
         SceneManager.LoadScene("MainMenu");
     }
 
     public void Restart()
     {
+        Cursor.visible = false;
         SceneManager.LoadScene("wwTests");
     }
 
